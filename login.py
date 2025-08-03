210cb90ff77de25b6ece4cfc1f0ade4b905ea3da
Roberto-Kaiser <159036437+Roberto-Kaiser@users.noreply.github.com>
2025-08-03 13:43:15 -0300

    Update login.py

--- login.py
+++ login.py
@@ -1,4 +1,3 @@
-# Dados de usuários cadastrados (email: senha)
 usuarios = {
     "admin@email.com": "admin123",
     "usuario@email.com": "senha123"
@@ -8,7 +7,6 @@ print("=== LOGIN ===")
 email = input("Email: ")
 senha = input("Senha: ")
 
-# Verifica se o email existe e se a senha está correta
 if email in usuarios and usuarios[email] == senha:
     print("\nLogin realizado com sucesso!")
 else:
